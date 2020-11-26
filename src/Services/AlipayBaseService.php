187c99da91ecdeb7847fb32f25da2bfff593118b
taoyl <492086163@qq.com>
2020-11-26 11:20:03 +0800

    增加文件夹

---
+++ src/Services/AlipayBaseService.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * The file is part of the ttglad/payment lib.
+ *
+ * (c) taoYl <snail_tyl@sina.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Paymenet\Services;
+
+/**
+ * Class AlipayBaseService
+ * @package Paymenet\Services
+ */
+abstract class AlipayBaseService extends BaseService
+{
+
+}
---
+++ src/Services/BaseService.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * The file is part of the ttglad/payment lib.
+ *
+ * (c) taoYl <snail_tyl@sina.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Paymenet\Services;
+
+/**
+ * Class BaseService
+ * @package Paymenet\Services
+ */
+abstract class BaseService
+{
+    const VERSION = '0.0.1';
+
+    /**
+     * @var Config
+     */
+    public static $config = null;
+
+    /**
+     * 获取版本号
+     * @return string
+     */
+    public static function getVersion()
+    {
+        return self::VERSION;
+    }
+
+    /**
+     * 获取类名称
+     * @return string
+     */
+    public function className()
+    {
+        return get_called_class();
+    }
+
+    /**
+     * 设置配置文件
+     * @param array $config
+     */
+    public function setConfig(array $config)
+    {
+        self::$config = new Config($config);
+    }
+}
